f29885c1e3891beb9e799cd7ba295d939bcaf68a
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-01-20 18:17:08 +0300

    Updated Bash completion file

--- sah_completion.bash
+++ sah_completion.bash
@@ -1,1 +1,1 @@
-complete -W "install update upgrade autoremove remove purge list custom stat config resetconfig pacconf mirrorlist updatemirrors changelog top browse log clearlog debug" sah
+complete -W "install update upgrade autoremove remove purge list custom stat config resetconfig pacconf mirrorlist updatemirrors changelog top cache browse view debug" sah
